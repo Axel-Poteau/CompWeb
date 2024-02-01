767e3d4fafedd1099952dd3b1f77aec356f81639
Alex Tournant <alex.tournant.1@gmail.com>
2024-02-01 12:58:36 +0100

    Commit #6

--- app/Http/Controllers/Api/SalleController.php
+++ app/Http/Controllers/Api/SalleController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Salle;
+use HttpResponseException;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Validator;
 
 class SalleController extends Controller
 {
@@ -12,7 +15,12 @@ class SalleController extends Controller
      */
     public function index()
     {
-        //
+        $salles=Salle::all();
+        return response()->json([
+            'status'=>true,
+            'message'=>"Salles Index successfully",
+            'salles'=>$salles
+        ],200);
     }
 
     /**
@@ -20,7 +28,17 @@ class SalleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $salle=new Salle();
+        $salle->nom=$request->nom;
+        $salle->adresse=$request->adresse;
+        $salle->code_postal=$request->code_postal;
+        $salle->ville=$request->ville;
+        $salle->save();
+        return response()->json([
+            'status'=>true,
+            'message'=>"Salle Store successfully",
+            'salle'=>$salle
+        ],200);
     }
 
     /**
@@ -28,7 +46,12 @@ class SalleController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $salle=Salle::findOrFail($id);
+        return response()->json([
+            'status'=>true,
+            'message'=>"Salles Show successfully",
+            'salles'=>$salle
+        ],200);
     }
 
     /**
@@ -36,7 +59,13 @@ class SalleController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $salle=Salle::findOrFail($id);
+        $salle->update($request->all());
+        return response()->json([
+            'status'=>true,
+            'message'=>"Salle Update successfully",
+            'salle'=>$salle
+        ],200);
     }
 
     /**
@@ -44,6 +73,12 @@ class SalleController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $salle=Salle::findOrFail($id);
+        $salle->delete();
+        return response()->json([
+            'status'=>true,
+            'message'=>"Salles Delete successfully",
+            'salle'=>$salle
+        ],200);
     }
 }
